a0460fb0b22e082c0ad371827581d98df384234e
SofiyaDesai13 <131664383+SofiyaDesai13@users.noreply.github.com>
2025-09-03 22:13:27 +0000

    03conversion

--- 01basics/03conversionOperation.js
+++ 01basics/03conversionOperation.js
@@ -15,5 +15,44 @@ let isLoggedIn = 1 //1 //"" //"hello Boss"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn); //true
 
+let someNumber = 45;
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//************************************Operations *************************************** */
+console.log("************************************");
+let value = 3
+let negValue = - value
+console.log(negValue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3) //2 raise to 3 = 8
+console.log(2/2) //quotient
+console.log(7%3) //remainder
+
+let str1 = "Sofiya"
+let str2 = " desai"
+console.log(str1+str2)
+console.log(1 + "2") //12
+console.log("1" + 2) //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+// console.log(+true); //1
+// console.log(+""); //0
+let gameCounter = 100
+gameCounter++ //101     //++gameCounter =101   
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
 
 
