803fababf4a55842535aae2601aa14eed62028b0
SofiyaDesai13 <131664383+SofiyaDesai13@users.noreply.github.com>
2025-09-03 17:04:51 +0000

    datatypes

---
+++ 01basics/01datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version of JS. Nowadays it comes automatically
+//alert(3+3)  we are using nodejs, not browser
+
+//console.log(3+3) console.log("Hello") //error
+
+console.log(3 
++3) //output 6, or but code-readability is disturbed 
+
+console.log(3+3); console.log("Hello")//output 6 Hello, or but code-readability is disturbed 
+
+let age =18;
+let isLoggedIn = false;
+let state;
+
+//number - 2^53
+//bigInt
+//string = ""
+//boolean - true/false
+//null - standalone value --> empty
+//undefined - value is not yet assigned
+//symbol - unique
+//object
+
+console.log(typeof "hyekj")
+console.log(typeof isLoggedIn)
+console.log(typeof null);  //output - object
+console.log(typeof undefined); //output - undefined
+
+
